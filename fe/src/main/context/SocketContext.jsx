7066ef4c3e5a46bd0bd1590e9fda8f558efcf034
Tai NV <tainv@liftsoft.vn>
2025-05-25 14:12:01 +0700

    update

--- fe/src/main/context/SocketContext.jsx
+++ fe/src/main/context/SocketContext.jsx
@@ -1,11 +1,20 @@
 import { createContext, useEffect, useState } from "react";
-import { endSession } from "../../web_qr/services/api";
+import { endSession, readSession } from "../../web_qr/services/api";
 
 // Create a context for the socket
 export const SocketContext = createContext(null);
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
+
+  const fetchUserProfile = async () => {
+    try {
+      const fetchSession = await readSession();
+      return fetchSession;
+    } catch (error) {
+      return null;
+    }
+  };
 
   // Ends the session and redirects the user
   const asyncEndSession = async () => {
@@ -24,7 +33,7 @@ export const SocketProvider = ({ children }) => {
 
   // Initialize WebSocket connection on mount
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:5001/ws/notifications/order/");
+    const ws = new WebSocket("ws://192.168.1.21:5001/ws/notifications/order/");
 
     ws.onopen = () => console.log("✅ WebSocket connected!");
     ws.onclose = (event) => console.log("❌ WebSocket disconnected!", event);
@@ -42,19 +51,21 @@ export const SocketProvider = ({ children }) => {
   useEffect(() => {
     if (!socket) return;
 
-    const handleMessage = (event) => {
+    const handleMessage = async (event) => {
       try {
         const data = JSON.parse(event.data);
-        console.log("📩 WebSocket message received:", data);
-
-        if (data?.type === "end_session") {
+        const session = await fetchUserProfile();
+        if (data?.type === "end_session" && session?.data?.session?.session_id === data?.data?.session_id) {
           asyncEndSession();
         }
+
         // Handle other message types here if needed
       } catch (err) {
         console.error("❌ Error parsing WebSocket message:", err);
       }
     };
+
+    socket.addEventListener("message", handleMessage);
 
     socket.addEventListener("message", handleMessage);
 
